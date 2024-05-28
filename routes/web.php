4c0e0e53de706b0838afa35308189b6eda5a7b50
Molzard <molzrdlegit@gmail.com>
2024-05-28 23:49:10 +0700

    hapus homo

--- routes/web.php
+++ routes/web.php
@@ -62,4 +62,4 @@ Route::get('pemesanan/add', [TransaksiController::class, 'create'])->name('trans
 
 Route::post('pemesanan/add/insert', [TransaksiController::class, 'store'])->name('transaksi.store');
 
-// halo alan alfaro homo banget lopyu
+// halo alan alfaro 
